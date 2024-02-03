28d1322421b2f01f956e75840c366254233a8851
Rajat Kumar <rajat9258@gmail.com>
2024-02-03 15:51:26 +0530

    AllPosts component bug fix

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -1,36 +1,28 @@
-import React,{useState, useEffect} from 'react'
-import { Container, PostCard } from '../component'
-import service from '../appwrite/config'
+import React, {useState, useEffect} from 'react'
+import { Container, PostCard } from '../component/index'
+import service from "../appwrite/config";
 
-const AllPost = () => {
-
+function AllPosts() {
     const [posts, setPosts] = useState([])
-    
-    useEffect(() => {
-    }, [])
-    service.getPosts([]).then((posts)=>{
-        if(posts)
-        {
+    useEffect(() => {}, [])
+    service.getPosts([]).then((posts) => {
+        if (posts) {
             setPosts(posts.documents)
         }
     })
-
   return (
     <div className='w-full py-8'>
         <Container>
             <div className='flex flex-wrap'>
-            { posts.map((post)=>(
-                <div key={post.$id} className='p-2 w-1/4'> 
-                <PostCard post={post}/>
-                </div>
-            ))
-
-            }
-
+                {posts.map((post) => (
+                    <div key={post.$id} className='p-2 w-1/4'>
+                        <PostCard {...post} />
+                    </div>
+                ))}
             </div>
-        </Container>
+            </Container>
     </div>
   )
 }
 
-export default AllPost
+export default AllPosts
